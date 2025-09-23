59c7b61b1618b975ef0ab820dbb980876e9b6e7d
Sidharth A T <sidharth.at@keyvalue.systems>
2025-09-24 00:52:46 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppRoutes from "./layout/AppRoutes";
+
+function App() {
+  return (
+    <div className="p-10">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,60 @@
+import { tv } from "tailwind-variants";
+
+const button = tv(
+  {
+    base: "flex m-1 items-center justify-center gap-x-2 rounded-lg text-sm font-medium leading-5 transition-all duration-300",
+    variants: {
+      color: {
+        primary: "bg-[#205BF1] text-white shadow-lg shadow-[#205BF1]/30",
+        secondary: "text-black bg-white border border-[#BBBBBB]",
+      },
+      size: {
+        normal: "px-10 py-2",
+        large: "p-4",
+        small: "p-2",
+      },
+      fullWidth: {
+        true: "w-full",
+      },
+      disabled: {
+        true: "text-white bg-gray-400 shadow-none border-0",
+      },
+    },
+    defaultVariants: {
+      color: "primary",
+      size: "normal",
+      fullWidth: false,
+    },
+  },
+  { twMerge: true }
+);
+
+const Button = ({
+  children,
+  className,
+  color,
+  disabled,
+  fullWidth,
+  icon,
+  size,
+  ...restProps
+}) => {
+  return (
+    <button
+      className={button({
+        className,
+        color,
+        disabled,
+        fullWidth,
+        size,
+      })}
+      disabled={disabled}
+      {...restProps}
+    >
+      {icon}
+      {children && <span>{children}</span>}
+    </button>
+  );
+};
+
+export default Button;
