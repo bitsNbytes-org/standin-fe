9a9f3f76317e2848b45ce1f20a3c745d4b5f3372
Sidharth A T <sidharth.at@keyvalue.systems>
2025-09-26 11:44:24 +0530

    added logo

--- Meeting-app/components/presentation.jsx
+++ Meeting-app/components/presentation.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { useRoomContext } from '@livekit/components-react';
-import { toastAlert } from '@/components/alert-toast';
+import PresentationLogo from '@/app/assets/images/presentation_logo.svg';
 
 const RenderPresentation = () => {
   const [currentData, setCurrentData] = useState({ heading: '', bullets: [] });
@@ -13,7 +14,8 @@ const RenderPresentation = () => {
         room.registerTextStreamHandler('chat', async (reader) => {
           const text = await reader.readAll();
           const parsedJson = JSON.parse(text);
-          setCurrentData(parsedJson.message);
+          // setCurrentData(parsedJson.message);
+          setCurrentData(parsedJson);
         });
       } catch (error) {
         console.log(error);
@@ -22,10 +24,10 @@ const RenderPresentation = () => {
   }, [room]);
 
   return (
-    <div className="p-10">
-      <p className="text-primary py-5 text-3xl font-bold">{currentData.heading}</p>
+    <div className="relative p-10">
+      <p className="text-primary py-5 text-3xl font-bold">{currentData?.heading}</p>
       <ul>
-        {currentData.bullets.map((bullet, bulletIndex) => (
+        {currentData?.bullets.map((bullet, bulletIndex) => (
           <li key={bulletIndex} className="flex p-4 text-2xl">
             <span className="text-primary animate-pulse pr-3 font-extrabold">•</span>
             <span className="pl-3 text-white transition-all duration-300 ease-in-out">
@@ -34,6 +36,9 @@ const RenderPresentation = () => {
           </li>
         ))}
       </ul>
+      <div className="absolute right-10 -bottom-20 rounded-2xl bg-white p-2">
+        <Image priority src={PresentationLogo} alt="Presentation Logo" width={100} height={100} />
+      </div>
     </div>
   );
 };
